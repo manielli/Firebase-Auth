668d8985f8fb0511f1e87cc13fca070dcb454294
Mani Jafari <manielli@gmail.com>
2019-03-19 15:52:11 -0700

    Fixed the removal of FormInput and FormLable from react-native-elements 1.1.0 to use Input instead

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { FormLabel, FormInput, Button } from 'react-native-elements';
+import { FormLabel, Input, Button } from 'react-native-elements';
 
 class SignUpForm extends Component {
     render() {
         return (
             <View>
                 <View style={{ marginBottom: 10 }} >
-                    <FormLabel>Enter Phone Number</FormLabel>
-                    <FormInput />
+                    <Input label="Enter your phone number" />
                 </View>
                 <Button title="Submit" />
             </View>
