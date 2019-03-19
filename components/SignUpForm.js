5bbc8984efba06ad3f6cb5dc2f0208533aba9d6a
Mani Jafari <manielli@gmail.com>
2019-03-19 16:17:05 -0700

    Added some ES7 shorthand syntax for bind(this)

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -10,16 +10,32 @@ class SignUpForm extends Component {
     //         phone: ''
     //     };
     // }
+    // This is ES7 (ES 2017) shorthand for the above
+    // They are identical
 
     state = { phone: '' };
+
+    // ES7 or ES 2017 for using it without .bind(this)
+    // as opposed to handleSubmit() { }
+    handleSubmit = () => {
+        
+    }
 
     render() {
         return (
             <View>
                 <View style={{ marginBottom: 10 }} >
-                    <Input label="Enter your phone number" />
+                    <Input 
+                        label="Enter your phone number"
+                        onChangeText={phone => this.setState({ phone })}    
+                    >
+                        {this.state.phone}
+                    </Input>
                 </View>
-                <Button title="Submit" />
+                <Button 
+                    onPress={this.handleSubmit}
+                    title="Submit" 
+                />
             </View>
         );
     }
