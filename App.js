94221aef754b6ab711d65a20cb8c09ebc909c668
Mani Jafari <manielli@gmail.com>
2019-03-19 18:42:41 -0700

    Installed firebase using npm install, added the config and firebase.initializeApp() to the App.js

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
+import config from './constants';
 import SignUpForm from './components/SignUpForm';
 import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: config.apiKey,
+      authDomain: config.authDomain,
+      databaseURL: config.databaseURL,
+      projectId: config.projectId,
+      storageBucket: config.storageBucket,
+      messagingSenderId: config.messagingSenderId
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
